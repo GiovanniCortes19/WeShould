1e8ef50e578220392c515397823fcefec141abfe
GiovanniCortes <giocortes19@gmail.com>
2023-11-07 15:33:43 -0500

    rendering React App on root of index.html with title of WeShould App

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,1 +1,13 @@
 // App Component
+import React from 'react';
+// import styles from '../styles.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="title">WeShould App</h1>
+    </div>
+  );
+};
+
+export default App;
--- client/index.js
+++ client/index.js
@@ -1,1 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+// import './styles.css';
+// IMPORT APP COMPONENT
+import App from './components/App.jsx';
 // RENDER REACT APP
+render(<App />, document.getElementById('root'));
