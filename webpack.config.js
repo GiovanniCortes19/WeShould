9be756f47c571eb942bf6f71d17c705971d96463
GiovanniCortes <giocortes19@gmail.com>
2023-11-07 12:35:21 -0500

    webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+// IMPORTS
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// CONFIGURE THE WEBPACK
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: 'defaults' }],
+              ['@babel/preset-react', { targets: 'defaults' }],
+            ],
+          },
+        },
+      },
+      {
+        test: /.css/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'client'),
+    },
+    port: 8080,
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+    }),
+  ],
+};
